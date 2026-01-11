fb09ff9f5f7b4eb32beed0b9f1eef7967040a7bb
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-01-11 23:19:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (27.60%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -20,21 +20,48 @@ public:
 
 // 2. Memoization + recursion 
     
-    int uniquePathCounts(int m, int n, vector<vector<int> >& pathCountsDp) {
+    // int uniquePathCounts(int m, int n, vector<vector<int> >& pathCountsDp) {
         
-        if(m == 0 && n == 0) return 1;
-        if(m < 0 || n < 0) return 0;
+    //     if(m == 0 && n == 0) return 1;
+    //     if(m < 0 || n < 0) return 0;
 
-        if(pathCountsDp[m][n] != -1) return pathCountsDp[m][n];
+    //     if(pathCountsDp[m][n] != -1) return pathCountsDp[m][n];
         
-        pathCountsDp[m][n] = uniquePathCounts(m, n-1, pathCountsDp) + uniquePathCounts(m-1, n, pathCountsDp);
+    //     pathCountsDp[m][n] = uniquePathCounts(m, n-1, pathCountsDp) + uniquePathCounts(m-1, n, pathCountsDp);
         
+    //     return pathCountsDp[m][n];
+    // }
+
+    // int uniquePaths(int m, int n) {
+        
+    //     vector<vector<int> > pathCountsDp(m, vector<int> (n, -1));
+    //     return uniquePathCounts(m-1, n-1, pathCountsDp);
+    // }
+
+// 3. Tabulation (iterative way)
+    
+    int uniquePathCounts(int m, int n, vector<vector<int> >& pathCountsDp) {
+
+        pathCountsDp[0][0] = 1;
+
+        for(int i = 0; i <= m; i++) {
+            for(int j = 0; j <= n; j++) {
+
+                if(i > 0 && j > 0) pathCountsDp[i][j] = pathCountsDp[i][j-1] + pathCountsDp[i-1][j];
+                
+                if(i == 0 && j > 0) pathCountsDp[i][j] += pathCountsDp[i][j-1];
+
+                if(i > 0 && j == 0) pathCountsDp[i][j] += pathCountsDp[i-1][j];
+            }
+        }
+
         return pathCountsDp[m][n];
     }
 
     int uniquePaths(int m, int n) {
         
-        vector<vector<int> > pathCountsDp(m, vector<int> (n, -1));
+        vector<vector<int> > pathCountsDp(m, vector<int> (n, 0));
         return uniquePathCounts(m-1, n-1, pathCountsDp);
     }
+
 };
