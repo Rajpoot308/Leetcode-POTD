31243cff3f1f22e8dc28df23e0f2b8847c0f4f4e
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-01-13 21:47:01 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (27.95%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -47,92 +47,92 @@ public:
 
 // 3. tabulation (iterative way)
     
-    // int countUniquePath(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& countPathDp) {
+    int countUniquePath(int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& countPathDp) {
 
-    //     if(obstacleGrid[m][n] == 1) return 0;
+        // if(obstacleGrid[m][n] == 1) return 0;
 
-    //     // countPathDp[0][0] = 1;
+        // countPathDp[0][0] = 1;
 
-    //     // for(int i = 0; i <= m; i++) {
-    //     //     for(int j = 0; j <= n; j++) {
+        // for(int i = 0; i <= m; i++) {
+        //     for(int j = 0; j <= n; j++) {
 
-    //     //         if(obstacleGrid[i][j] != 1) {
+        //         if(obstacleGrid[i][j] != 1) {
 
-    //     //             if(i > 0 && j > 0 && (obstacleGrid[i-1][j] != 1) &&)
-    //     //         }
-    //     //     }
-    //     // }
-
-    //     for(int i = 0; i <= m; i++) {
-    //         if(obstacleGrid[i][0] == 1) {
-    //             break;
-    //         }
-    //         countPathDp[i][0] = 1;
-    //     }
-
-    //     for(int j = 0; j <= n; j++) {
-    //         if(obstacleGrid[0][j] == 1) {
-    //             break;
-    //         }
-    //         countPathDp[0][j] = 1;
-    //     }
-
-    //     for(int i = 1; i <= m; i++) {
-    //         for(int j = 1; j <= n; j++) {
-
-    //             if(obstacleGrid[i][j] != 1) {
-    //                 countPathDp[i][j] = countPathDp[i-1][j] + countPathDp[i][j-1];
-    //             }
-
-    //         }
-    //     }
-
-    //     return countPathDp[m][n];
-    // }
-
-    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-    //     int m = obstacleGrid.size();
-    //     int n = obstacleGrid[0].size();
-
-    //     vector<vector<int>> countPathDp(m, vector<int> (n, 0));
-    //     return countUniquePath(m-1, n-1, obstacleGrid, countPathDp);
-    // }
-
-// 4. space optimaization
-    
-    int countUniquePath(int m, int n, vector<vector<int>>& obstacleGrid) {
-
-        // if(obstacleGrid[m][n] == 1 || obstacleGrid[0][0] == 1) return 0;
-
-        vector<int> prev(n+1, 0);
+        //             if(i > 0 && j > 0 && (obstacleGrid[i-1][j] != 1) &&)
+        //         }
+        //     }
+        // }
 
         for(int i = 0; i <= m; i++) {
-            vector<int> curr(n+1, 0);
-            for(int j = 0; j <= n; j++) {
-
-                if(obstacleGrid[i][j] != 1) {
-                    
-                    if(i == 0 && j == 0) {
-                        curr[j] = 1;
-                    }
-                    else {
-                        int left = j > 0 ? curr[j-1] : 0;
-                        int up = prev[j];
-                        curr[j] = left + up;
-                    }
-                }
+            if(obstacleGrid[i][0] == 1) {
+                break;
             }
-            prev = curr;
+            countPathDp[i][0] = 1;
         }
 
-        return prev[n];
+        for(int j = 0; j <= n; j++) {
+            if(obstacleGrid[0][j] == 1) {
+                break;
+            }
+            countPathDp[0][j] = 1;
+        }
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+
+                if(obstacleGrid[i][j] != 1) {
+                    countPathDp[i][j] = countPathDp[i-1][j] + countPathDp[i][j-1];
+                }
+
+            }
+        }
+
+        return countPathDp[m][n];
     }
 
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        return countUniquePath(m-1, n-1, obstacleGrid);
+        vector<vector<int>> countPathDp(m, vector<int> (n, 0));
+        return countUniquePath(m-1, n-1, obstacleGrid, countPathDp);
     }
+
+// 4. space optimaization
+    
+    // int countUniquePath(int m, int n, vector<vector<int>>& obstacleGrid) {
+
+    //     // if(obstacleGrid[m][n] == 1 || obstacleGrid[0][0] == 1) return 0;
+
+    //     vector<int> prev(n+1, 0);
+
+    //     for(int i = 0; i <= m; i++) {
+    //         vector<int> curr(n+1, 0);
+    //         for(int j = 0; j <= n; j++) {
+
+    //             if(obstacleGrid[i][j] != 1) {
+                    
+    //                 if(i == 0 && j == 0) {
+    //                     curr[j] = 1;
+    //                 }
+    //                 else {
+    //                     int left = j > 0 ? curr[j-1] : 0;
+    //                     int up = prev[j];
+    //                     curr[j] = left + up;
+    //                 }
+    //             }
+    //         }
+    //         prev = curr;
+    //     }
+
+    //     return prev[n];
+    // }
+
+    // int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    //     int m = obstacleGrid.size();
+    //     int n = obstacleGrid[0].size();
+
+    //     return countUniquePath(m-1, n-1, obstacleGrid);
+    // }
     
 };
