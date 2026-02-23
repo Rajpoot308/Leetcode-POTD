a92e05c3a411aec2dab1b61d607352fa19a98967
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-02-23 21:41:19 +0530

    Time: 44 ms (68.34%) | Memory: 90.9 MB (44.03%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -15,16 +15,42 @@ public:
     // }
 
 // 2. sorting T.C. O(nlogn) S.C. O(1)
+    // bool containsDuplicate(vector<int>& nums) {
+    //     int n = nums.size();
+    //     sort(nums.begin(), nums.end());
+
+    //     for(int i = 1; i < n; i++) {
+    //         if(nums[i] == nums[i-1])
+    //          return true;
+    //     }
+
+    //     return false;
+    // }
+
+// 3. using hashset T.C. O(n) S.C. O(n)
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
+       int n = nums.size();
+       unordered_set<int> look;
+       for(int num : nums) {
+          if(look.count(num) > 0) 
+            return true;
 
-        for(int i = 1; i < n; i++) {
-            if(nums[i] == nums[i-1])
-             return true;
-        }
+            look.insert(num);
+       }
 
-        return false;
+       return false;
     }
+
+// 4. using hash map 
+    // bool containsDuplicate(vector<int>& nums) {
+    //    int n = nums.size();
+    //    unordered_map<int> look;
+    //    for(int num : nums) {
+    //       if(look.count(num) > 0) 
+    //         return true;
+    //    }
+
+    //    return false;
+    // }
 
 };
