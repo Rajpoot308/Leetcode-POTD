44a2fc5b85b3add81739aa109c1cb6179023c6a6
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-02-24 12:36:27 +0530

    Time: 1 ms (13.54%) | Memory: 17.1 MB (17.38%) - LeetSync

--- 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
+++ 2093-check-if-string-is-a-prefix-of-array/check-if-string-is-a-prefix-of-array.cpp
@@ -3,30 +3,45 @@ public:
 
 // 1. bruteforce
     
-    bool isPrefix(string& target, string& source) {
-        int i = 0, j = 0;
-        while(i < target.length() && j < source.length()) {
-            if(target[i] != source[j]) {
-                return false;
-            }
-            i++;
-            j++;
-        }
-        return true;
-    }
+    // bool isPrefix(string& target, string& source) {
+    //     int i = 0, j = 0;
+    //     while(i < target.length() && j < source.length()) {
+    //         if(target[i] != source[j]) {
+    //             return false;
+    //         }
+    //         i++;
+    //         j++;
+    //     }
+    //     return true;
+    // }
     
-    bool isPrefixString(string s, vector<string>& words) {
+    // bool isPrefixString(string s, vector<string>& words) {
         
+    //     string samStr = "";
+    //     for(string& str : words) {
+    //         samStr += str;
+
+    //         if(!isPrefix(samStr, s)) {
+    //             break;
+    //         }
+    //         if(samStr == s) return true;
+
+    //     }
+    //     return false;
+    // }
+
+// 2. length of concatinated of k strings in words must exactly equal to the length of the s string
+    bool isPrefixString(string s, vector<string>& words) {
+        int n = s.length();
         string samStr = "";
+
         for(string& str : words) {
             samStr += str;
-
-            if(!isPrefix(samStr, s)) {
-                break;
-            }
-            if(samStr == s) return true;
+            if((samStr.size() == n) && (samStr == s)) return true;
 
         }
+
         return false;
+    
     }
 };
