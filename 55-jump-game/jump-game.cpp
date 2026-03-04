d15625049e235262c5c510f8b55cf85f649192b4
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-03-04 18:38:24 +0530

    Time: 4 ms (22.52%) | Memory: 52.2 MB (95.59%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -22,29 +22,43 @@ public:
     // }
 
 // 2. Recursion + Memoization (iterative way) 
-    bool canReachAtLast(vector<int>& nums, int i, int n, vector<int>& dp) {
-        if(i == n) return true;
+    // bool canReachAtLast(vector<int>& nums, int i, int n, vector<int>& dp) {
+    //     if(i == n) return true;
         
-        if(dp[i] != -1) {
-            return dp[i];
-        }
+    //     if(dp[i] != -1) {
+    //         return dp[i];
+    //     }
 
-        for(int j = nums[i]; j > 0; j--) {
+    //     for(int j = nums[i]; j > 0; j--) {
         
-            if(i + j <= n) {
-                dp[i+j] = canReachAtLast(nums, i+j, n, dp);
-                if(dp[i+j] == 1) return dp[i+j];
-            }
-        }
+    //         if(i + j <= n) {
+    //             dp[i+j] = canReachAtLast(nums, i+j, n, dp);
+    //             if(dp[i+j] == 1) return dp[i+j];
+    //         }
+    //     }
 
-        return false;
-    }
+    //     return false;
+    // }
 
+    // bool canJump(vector<int>& nums) {
+    //     int start = 0;
+    //     int last = nums.size()-1;
+    //     vector<int> dp(last + 1, -1);
+
+    //     return canReachAtLast(nums, start, last, dp);
+    // }
+
+
+// 3. 
     bool canJump(vector<int>& nums) {
-        int start = 0;
-        int last = nums.size()-1;
-        vector<int> dp(last + 1, -1);
+        int n = nums.size();
+        int last = n-1;
 
-        return canReachAtLast(nums, start, last, dp);
+        for(int i = n-2; i >= 0; i--) {
+            if(last-i <= nums[i]) last = i;
+        }
+
+        if(last == 0) return true;
+        return false;
     }
 };
