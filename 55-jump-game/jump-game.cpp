79b3f30ad3567026e5d8f56937f7a2d757b3b572
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-03-04 20:33:47 +0530

    Time: 620 ms (6.70%) | Memory: 54.9 MB (9.33%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-// 1. Recursion + Greedy 
+// 1. Recursion
     
     // bool canReachAtLast(vector<int>& nums, int i, int n) {
     //     if(i == n) return true;
@@ -48,17 +48,58 @@ public:
     //     return canReachAtLast(nums, start, last, dp);
     // }
 
+// 3. recursion + tabulation(dp) 
 
-// 3. 
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int last = n-1;
+        vector<int> dp(n, -1);
+        dp[n-1] = 1;
 
         for(int i = n-2; i >= 0; i--) {
-            if(last-i <= nums[i]) last = i;
+
+            bool flag = true;
+            for(int j = nums[i]; j > 0; j--) {
+                if(i+j < n && dp[i+j]) {
+                    dp[i] = 1;
+                    flag = false;
+                    break;
+                }
+            }
+
+            if(flag) dp[i] = 0;
         }
 
-        if(last == 0) return true;
-        return false;
-    }
+        return dp[0];
+    } 
+
+
+
+// 4. 
+    // bool canJump(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int last = n-1;
+
+    //     for(int i = n-2; i >= 0; i--) {
+    //         if(last-i <= nums[i]) last = i;
+    //     }
+
+    //     if(last == 0) return true;
+    //     return false;
+    // }
+
+// 4. Greedy - Greedy to jump maximum T.C. O(n) S.C. O(1) 
+    /* One catch in this problem is that if there is no 0 present in the array then 
+       it will be always possible to reach at the end */
+
+    // bool canJump(vector<int>& nums) {
+    //     int maxJump = 0;
+
+    //     for(int i = 0; i < nums.size(); i++) {
+    //         if(i > maxJump) return false;
+
+    //         maxJump = max(maxJump, i + nums[i]);
+    //     }
+
+    //     return true;
+    // }
 };
