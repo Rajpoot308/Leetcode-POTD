131c1ac0f70a2e987819fe4a52da5ce90ed87730
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-03-06 16:50:29 +0530

    Time: 52 ms (22.40%) | Memory: 21.5 MB (24.27%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -28,32 +28,56 @@ public:
     // }
 
 
-// 2. recursion + dp(iterative way) 
-    int computeMinimumJump(vector<int>& nums, int i, int n, vector<int>& dp) {
-        if(i == n-1) {
-           return 0;
-        }
+// 2. recursion + Memoization dp(iterative way) 
+    // int computeMinimumJump(vector<int>& nums, int i, int n, vector<int>& dp) {
+    //     if(i == n-1) {
+    //        return 0;
+    //     }
 
-        if(dp[i] != -1) return dp[i];
+    //     if(dp[i] != -1) return dp[i];
 
-        int minJump = n-1;
+    //     int minJump = n-1;
 
-        for(int j = i + 1; j <= i + nums[i]; j++) {
+    //     for(int j = i + 1; j <= i + nums[i]; j++) {
 
-            if(j < n) {
-                int totalJump = 1 + computeMinimumJump(nums, j, n, dp);
-                minJump = min(minJump, totalJump);
-            }
-        }
+    //         if(j < n) {
+    //             int totalJump = 1 + computeMinimumJump(nums, j, n, dp);
+    //             minJump = min(minJump, totalJump);
+    //         }
+    //     }
 
-        dp[i] = minJump;
-        return dp[i];
-    }
+    //     dp[i] = minJump;
+    //     return dp[i];
+    // }
+    // int jump(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int startInd = 0;
+    //     vector<int> dp(n, -1);
+
+    //     return computeMinimumJump(nums, startInd, n, dp);
+    // }
+
+// 3. recursion + dp(tabulation)
     int jump(vector<int>& nums) {
         int n = nums.size();
         int startInd = 0;
-        vector<int> dp(n, -1);
+        vector<int> dp(n, n-1);
 
-        return computeMinimumJump(nums, startInd, n, dp);
+        dp[n-1] = 0;
+        
+        for(int i = n-2; i >= 0; i--) {
+            int minJump = n-1;
+
+            for(int j = i + 1; j <= i + nums[i]; j++) {
+                if(j < n && (dp[j] != n-1)) {
+                    int totalJump = 1 + dp[j];
+                    minJump = min(minJump, totalJump);
+                }
+            }
+
+           dp[i] = minJump;            
+        }
+
+        return dp[0];
     }
 };
