4835ba543d99a0dbac262c0c35b973921ced9be8
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-03-06 18:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (61.88%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-// 1. Recursion T.C. O(n2) S.C. auxilary space O(n) 
+// 1. Recursion T.C. O(n^n) S.C. auxilary space O(n) 
 
     // int computeMinimumJump(vector<int>& nums, int i, int n) {
     //     if(i == n-1) {
@@ -28,7 +28,7 @@ public:
     // }
 
 
-// 2. recursion + Memoization dp(iterative way) 
+// 2. recursion + Memoization dp(iterative way) T.C. O(n^2) S.C. O(n) + O(n)
     // int computeMinimumJump(vector<int>& nums, int i, int n, vector<int>& dp) {
     //     if(i == n-1) {
     //        return 0;
@@ -57,27 +57,48 @@ public:
     //     return computeMinimumJump(nums, startInd, n, dp);
     // }
 
-// 3. recursion + dp(tabulation)
+// 3. recursion + dp(tabulation) T.C. O(n^2) S.C. O(n) 
+    // int jump(vector<int>& nums) {
+    //     int n = nums.size();
+    //     int startInd = 0;
+    //     vector<int> dp(n, n-1);
+
+    //     dp[n-1] = 0;
+        
+    //     for(int i = n-2; i >= 0; i--) {
+    //         int minJump = n-1;
+
+    //         for(int j = i + 1; j <= i + nums[i]; j++) {
+    //             if(j < n && (dp[j] != n-1)) {
+    //                 int totalJump = 1 + dp[j];
+    //                 minJump = min(minJump, totalJump);
+    //             }
+    //         }
+
+    //        dp[i] = minJump;            
+    //     }
+
+    //     return dp[0];
+    // }
+
+// 4. Greedy (range based method) T.C. O(n) S.C. O(1)
     int jump(vector<int>& nums) {
         int n = nums.size();
-        int startInd = 0;
-        vector<int> dp(n, n-1);
 
-        dp[n-1] = 0;
+        int jump = 0;
+        int l = 0, r = 0, i = 0;
         
-        for(int i = n-2; i >= 0; i--) {
-            int minJump = n-1;
-
-            for(int j = i + 1; j <= i + nums[i]; j++) {
-                if(j < n && (dp[j] != n-1)) {
-                    int totalJump = 1 + dp[j];
-                    minJump = min(minJump, totalJump);
-                }
+        while(i < n-1) {
+            int extreme = 0;
+            for(int j = l; j <= r; j++) {
+                extreme = max(j + nums[j], extreme);
             }
-
-           dp[i] = minJump;            
+            l = i + 1;
+            r = extreme;
+            i = r;
+            jump++;
         }
 
-        return dp[0];
+        return jump;
     }
 };
