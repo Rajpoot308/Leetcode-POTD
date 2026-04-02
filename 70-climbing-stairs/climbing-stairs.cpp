4a6e4b901e82ea989923ad4b1b2912df23852091
Aman Rajpoot <125914102+Rajpoot308@users.noreply.github.com>
2026-04-02 12:54:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (17.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 
-// 1. bottom to top recursion 
+// 1. bottom to top recursion T.C. O(2^n) S.C. O(n) auxilary space
     
     // int totalDistinctWays(int currStep, int n) {
         
@@ -24,27 +24,53 @@ public:
     //     return totalDistinctWays(currStep, n);
     // }
 
-// 2. recursion + memoization
-    int totalDistinctWays(int currStep, int n, vector<int>& dp) {
+// 2. recursion + memoization  T.C. O(n) S.C. O(n) + O(n) recursive stack
+    // int totalDistinctWays(int currStep, int n, vector<int>& dp) {
         
-        if(currStep == n) return 1;
-        if(currStep > n) return 0;
+    //     if(currStep == n) return 1;
+    //     if(currStep > n) return 0;
 
-        if(dp[currStep] != -1) return dp[currStep];
+    //     if(dp[currStep] != -1) return dp[currStep];
 
-        int count = 0;
-        for(int i = 1; i < 3; i++) {
-            int subCount = totalDistinctWays(currStep + i, n, dp); 
-            count = count + subCount;
+    //     int count = 0;
+    //     for(int i = 1; i < 3; i++) {
+    //         int subCount = totalDistinctWays(currStep + i, n, dp); 
+    //         count = count + subCount;
+    //         dp[currStep] = count;
+    //     }
+
+    //     return dp[currStep];
+    // }
+     
+    // int climbStairs(int n) {
+    //     int currStep = 0;
+    //     vector<int> dp(n, -1);
+    //     return totalDistinctWays(currStep, n, dp);
+    // }
+
+// 3. iterative way T.C. O(n) S.C. O(n) and no recursive stack
+    int totalDistinctWays(int n, vector<int>& dp) {
+        
+        if(n > 0) dp[1] = 1;
+        if(n > 1) dp[2] = 2;
+
+        
+        for(int currStep = 3; currStep <= n; currStep++) {
+            int count = 0;
+            for(int i = 1; i < 3; i++) {
+                int subCount = dp[currStep - i]; 
+                count = count + subCount;  
+            }
             dp[currStep] = count;
         }
+        
 
-        return dp[currStep];
+        return dp[n];
     }
      
     int climbStairs(int n) {
-        int currStep = 0;
-        vector<int> dp(n, -1);
-        return totalDistinctWays(currStep, n, dp);
+
+        vector<int> dp(n + 1, 0);
+        return totalDistinctWays(n, dp);
     }
 };
